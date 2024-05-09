69a3560878948a6749b1ee0554d49793e120755c
Hyeona01 <sha0209@dgu.ac.kr>
2024-05-09 16:53:20 +0900

    🎨 format: style 파일

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,69 @@
+import { styled } from "styled-components";
+import { GlobalStyle } from "./style/globalStyle";
+import { Outlet, useLocation } from "react-router-dom";
+import Footer from "./components/layouts/footer/Footer";
+import Header from "./components/layouts/header/Header";
+
+const BackGroundColor = styled.div`
+  width: 100vw;
+  min-height: 100vh;
+  flex-direction: column;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: black;
+`;
+
+// 기본 view 설정 정의
+const Wrapper = styled.div` 
+  flex-grow: 1;
+  padding-top: 60px;
+  margin: 0 auto;
+  width: 100%;
+  max-width: 420px;
+  
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  background-color: white;
+  font-family: NotoSansRegular;
+  color: white;
+`;
+
+const Content = styled.div`
+  flex-grow: 1;
+  min-height: 100%;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Layout = () => {
+  const location = useLocation();
+
+  return (
+    <BackGroundColor>
+      <Header />
+      {/* {location.pathname !== "/" && <Header />} */}
+      <Wrapper>
+        <Content>
+          <Outlet />
+        </Content>
+        <Footer />
+      </Wrapper>
+    </BackGroundColor>
+  );
+};
+
 function App() {
 
   return (
     <>
-    <div>app.js</div>
+    <GlobalStyle />
+    <Layout />
     </>
   )
 }
---
+++ src/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <>
+    <div style={'color : red'}>NotFound</div>
+    <div style={'color : pink'}>애도라 화잇팅이얏 XD</div>
+    </>
+  )
+}
+
+export default NotFound
