67ab8f145e5ca15d582f5460bf82ffb6f599f9db
ZZZINU <nnjin987@gmail.com>
2024-05-19 04:47:39 +0900

    ✨ Feat: 부스 페이지 - 날짜 selector 구현

--- src/pages/booth/Booth.jsx
+++ src/pages/booth/Booth.jsx
@@ -1,11 +1,13 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import DataSelector from '../../components/booth/dateSelector/DateSelector';
 
 const Booth = () => {
   return (
     <>
-      <div>Booth</div>
-      <Link to={"/booths/1"}>detail 이동</Link>
+      {/* <div>Booth</div>
+      <Link to={'/booths/1'}>detail 이동</Link> */}
+      <DataSelector />
     </>
   );
 };
