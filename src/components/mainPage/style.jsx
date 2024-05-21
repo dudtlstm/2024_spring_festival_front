010c7e4f4788d4abc886b39c9f9b5eb8b364b3ca
coldweek3 <ohchanju3@naver.com>
2024-05-22 06:52:45 +0900

    :sparkles: 메인 페이지 - 부스 랭킹

--- src/components/mainPage/style.jsx
+++ src/components/mainPage/style.jsx
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 
+//title.jsx
 export const TitleWrapper = styled.div`
   display: flex;
   flex-direction: row;
   margin-top: 50px;
   align-items: center;
   justify-content: space-between;
-  padding-right: 20px;
+  padding: 20px;
 `;
 
 export const TitleText = styled.div`
@@ -20,6 +21,8 @@ export const MoreInfo = styled.div`
   font-weight: 500;
   font-size: 12px;
 `;
+
+//lineup.jsx
 
 export const LineUpWrapper = styled.div`
   width: 100%;
@@ -39,4 +42,104 @@ export const LineUpImgContainer = styled.div`
 
 export const LineUpImg = styled.img`
   width: 150px;
+`;
+
+//BoothRank.jsx
+
+export const BoothRankWrapper = styled.div`
+  /* border: 1px solid red; */
+`;
+
+export const BoothRankBg = styled.div`
+  background-color: #fbfbfb;
+  width: 100%;
+  padding: 20px;
+  gap: 20px;
+  display: flex;
+  flex-direction: column;
+`;
+
+//BoothRankCard.jsx
+
+export const BoothRankCardWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 8px;
+`;
+
+export const BoothRankNum = styled.div`
+  width: 20px;
+  height: 24px;
+`;
+
+export const BoothRankNumImg = styled.img`
+  width: 100%;
+`;
+
+export const BoothThumImg = styled.img`
+  width: 90px;
+  height: 90px;
+  border-radius: 8px;
+`;
+
+export const BoothRankInfo = styled.div`
+  width: 50%;
+`;
+
+export const BoothRankTitle = styled.div`
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 600;
+`;
+
+export const BoothRankHeart = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  color: #f97c69;
+  margin-top: 4px;
+  gap: 2px;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 500;
+`;
+
+export const BoothHeartImg = styled.img`
+  width: 16px;
+  height: 16px;
+`;
+
+export const BoothRankDes = styled.div`
+  margin-top: 10px;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 16px;
+  color: #929292;
+`;
+
+export const BoothMore = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 28px 0px 24px 0px;
+  background-color: #f1f1f1;
+  border-radius: 8px;
+  justify-content: center;
+  align-items: center;
+  width: 52px;
+  height: 90px;
+  cursor: pointer;
+`;
+
+export const BoothMoreArrow = styled.img`
+  width: 24px;
+  height: 24px;
+`;
+
+export const BoothMoreText = styled.div`
+  font-size: 10px;
+  font-style: normal;
+  font-weight: 400;
+  color: #c4c4c4;
 `;
--- src/pages/mainpage/Mainpage.jsx
+++ src/pages/mainpage/Mainpage.jsx
@@ -3,12 +3,14 @@ import React from "react";
 //import component
 import TopBanner from "../../components/mainPage/TopBanner";
 import LineUp from "../../components/mainPage/LineUp";
+import BoothRank from "../../components/mainPage/BoothRank";
 
 const Mainpage = () => {
   return (
     <>
       <TopBanner />
       <LineUp />
+      <BoothRank />
     </>
   );
 };
