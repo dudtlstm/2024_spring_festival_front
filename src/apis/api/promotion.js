489a2bc3604297f795fd3f6bbbd004e1563f39c7
dudtlstm <annpys1024@naver.com>
2024-05-22 14:29:49 +0900

    ✨ feat: promotion 구현 완료

--- src/apis/api/promotion.js
+++ src/apis/api/promotion.js
@@ -1,14 +1,13 @@
 import { API } from "../utils";
 
 // 수정하여 사용하세요!
-export const promotion = async () => {
+export const getPromotions = async () => {
   try {
-    const response = await API.get('', {
-    });
-    console.log("success!");
-    return response;
-
+    const response = await API.get('/api/v1/promotion');
+    console.log("success!", response.data);
+    return Array.isArray(response.data) ? response.data : [];
   } catch (error) {
     console.error("API error: ", error);
+    throw error;
   }
 };
