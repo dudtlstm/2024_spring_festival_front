ebfdf1b01bb2b72e42448f08c3d77fd590e4d9c8
dudtlstm <annpys1024@naver.com>
2024-05-24 19:33:39 +0900

    💄 style: footer 수정 완료

--- src/components/layouts/footer/Footer.jsx
+++ src/components/layouts/footer/Footer.jsx
@@ -1,32 +1,40 @@
 import React from "react";
 import * as S from "./style";
-import { Link } from "react-router-dom";
 import * as images from "../../../assets/images/layout_images";
 
 const Footer = () => {
   return (
     <S.FooterWrapper>
       <S.Copyright>
-        동국대학교 멋쟁이사자처럼
-        <br />
-        @DGU LikeLion. All rights Reserved.
+        <S.LIKELIONDGU>
+          동국대학교 멋쟁이사자처럼
+          <br />
+          @LIKELION DGU. All rights Reserved.
+        </S.LIKELIONDGU>
         <S.DesignSupport>@Design supported by 축제기획단</S.DesignSupport>
       </S.Copyright>
+      
       <S.ContectContainer>
         <S.IconContainer>
-          <img src={images.footerlionImg} alt="developers" />
-          <Link to="/about">Developers</Link>
-        </S.IconContainer>
-        <S.IconContainer>
-          <img src={images.github} alt="github" />
-          <Link to="https://github.com/LikeLion-at-DGU">Github</Link>
-        </S.IconContainer>
-        <S.IconContainer>
-          <img src={images.instagram} alt="instagram" />
-          <Link to="https://www.instagram.com/likelion_dongguk/">
-            Instagram
-          </Link>
-        </S.IconContainer>
+            <img src={images.github} alt="github" />
+            <a href="https://github.com/LikeLion-at-DGU" target="_blank" rel="noopener noreferrer">
+              Github
+            </a>
+          </S.IconContainer>
+          <S.VerticalLine />
+          <S.IconContainer>
+            <img src={images.footerlionImg} alt="developers" />
+            <a href="/about" target="_blank" rel="noopener noreferrer">
+              Developers
+            </a>
+          </S.IconContainer>
+          <S.VerticalLine />
+          <S.IconContainer>
+            <img src={images.instagram} alt="instagram" />
+            <a href="https://www.instagram.com/likelion_dongguk/" target="_blank" rel="noopener noreferrer">
+              Instagram
+            </a>
+          </S.IconContainer>
       </S.ContectContainer>
     </S.FooterWrapper>
   );
--- src/components/layouts/footer/style.jsx
+++ src/components/layouts/footer/style.jsx
@@ -2,45 +2,71 @@ import { styled } from "styled-components";
 
 export const FooterWrapper = styled.div`
   width: 100%;
-  height: 210px;
+  height: 220px;
 
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
 
-  gap: 18px;
-  padding-bottom: 72px; /* nav */
+  gap: 20px;
+
+  padding: 20px 51px 34px 51px;
 
   background: var(--use-background-background---section, #fbfbfb);
   color: var(--use-font-font---info, #929292);
   font-family: Pretendard;
   font-size: 12px;
 `;
+
 export const Copyright = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+`;
+
+export const LIKELIONDGU = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 4px;
+
   font-style: normal;
   font-weight: 400;
+  font-size: 12px;
   line-height: 22px;
   text-align: center;
 `;
+
 export const DesignSupport = styled.div`
   color: var(--use-font-font---disable, #c4c4c4);
   font-family: Pretendard;
   font-size: 10px;
   font-weight: 400;
-  line-height: 14px;
+  line-height: 16px;
 `;
 
+// 컨택트
 export const ContectContainer = styled.div`
-  width: 100%;
-  padding: 0 46px;
   display: flex;
-  justify-content: space-around;
+  justify-content: center;
+  align-items: center;
+  gap: 12px;
 `;
+
+export const VerticalLine = styled.div`
+  width: 1px;
+  height: 8px;
+  background: #E2E3EC;
+`;
+
 export const IconContainer = styled.div`
   display: flex;
   align-items: center;
-  gap: 6px;
+  justify-content: center;
+  text-align: center;
+  gap: 4px;
+  width: 80px;
 
   color: var(--use-font-font---info, #929292);
 
