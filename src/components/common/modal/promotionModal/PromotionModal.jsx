fb63b9b6dd2ebc928d83f9b7b757cb89ca15f878
coldweek3 <ohchanju3@naver.com>
2024-05-24 15:42:12 +0900

    :art: 메인페이지 배너 수정

--- src/components/common/modal/promotionModal/PromotionModal.jsx
+++ src/components/common/modal/promotionModal/PromotionModal.jsx
@@ -9,29 +9,6 @@ function PromotionModal({ isOpen, onClose, onConfirm, description, title }) {
       contentLabel="사이트 연결 확인"
       ariaHideApp={false}
       shouldCloseOnOverlayClick={true}
-      overlayClassName="customOverlay"
-    >
-      <S.SiteConnectWrapper>
-        <S.SiteConnect>
-          <S.SiteConnectTitle>{title}</S.SiteConnectTitle>
-          <S.SiteConnectContent>{description}</S.SiteConnectContent>
-          <S.SiteConnectButton>
-            <S.SiteConnectCancle onClick={onClose}>취소</S.SiteConnectCancle>
-            <S.SiteConnectConfirm onClick={onConfirm}>
-              확인
-            </S.SiteConnectConfirm>
-          </S.SiteConnectButton>
-        </S.SiteConnect>
-      </S.SiteConnectWrapper>
-    </S.IsModal>
-  );
-  return (
-    <S.IsModal
-      isOpen={isOpen}
-      onRequestClose={onClose}
-      contentLabel="사이트 연결 확인"
-      ariaHideApp={false}
-      shouldCloseOnOverlayClick={true}
       zIndex={200}
     >
       <S.SiteConnectWrapper>
