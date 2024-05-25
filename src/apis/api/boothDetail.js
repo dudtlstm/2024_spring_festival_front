c2375daca6f2e2fdca1beb39d325bcf66d31da15
ZZZINU <nnjin987@gmail.com>
2024-05-26 02:08:52 +0900

    ✨ Feat: 부스 페이지 - 부스 디테일 API date 전달 보류

--- src/apis/api/boothDetail.js
+++ src/apis/api/boothDetail.js
@@ -2,7 +2,7 @@ import { API } from '../utils';
 
 export const boothDetail = async (id, date) => {
   try {
-    const response = await API.get(`api/v1/booth/${id}?date=${date}`);
+    const response = await API.get(`api/v1/booth/${id}`);
     return response.data;
   } catch (error) {
     // console.error('API error: ', error);
