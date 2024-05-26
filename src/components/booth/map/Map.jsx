bf22fdf2c9385cd840aed82894505d192eced75f
ZZZINU <nnjin987@gmail.com>
2024-05-26 18:38:43 +0900

    🔥 Remove: 부스 페이지 - 맵 더미 데이터 및 콘솔 로그 제거

--- src/components/booth/map/Map.jsx
+++ src/components/booth/map/Map.jsx
@@ -4,25 +4,7 @@ import MarkerBooth from '../../../../public/booth/marker_booth.svg';
 import MarkerFood from '../../../../public/booth/marker_food.svg';
 import MarkerMarket from '../../../../public/booth/marker_market.svg';
 
-const dummyData = [
-  {
-    id: 1,
-    name: '이름1',
-    location: '위치1',
-    latitude: 37.558288,
-    longitude: 127.000173,
-  },
-  {
-    id: 2,
-    name: '이름2',
-    location: '위치2',
-    latitude: 37.54699,
-    longitude: 127.09598,
-  },
-];
-
 const Map = ({ data, category, selectedBoothId, resetData, onMarkerClick }) => {
-  console.log('잘 받고있니>?', data);
   const [map, setMap] = useState();
   const markersRef = useRef([]);
 
@@ -96,10 +78,6 @@ const Map = ({ data, category, selectedBoothId, resetData, onMarkerClick }) => {
         });
 
         if (selectedBoothId === item.id) {
-          console.log(
-            'item.location_info.longitude: ',
-            item.location_info.longitude
-          );
           map.setCenter(
             new kakao.maps.LatLng(
               item.location_info.latitude,
