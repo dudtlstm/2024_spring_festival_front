4fa4fedb48b92fda974f528ea02e5551e5665230
Hyeona01 <sha0209@dgu.ac.kr>
2024-05-26 18:58:36 +0900

    🔥 remove: console log

--- src/apis/api/lineup.js
+++ src/apis/api/lineup.js
@@ -3,11 +3,11 @@ import { API } from "../../apis/utils";
 export const fetchArtists = async (day) => {
   try {
     const response = await API.get(`api/v1/artist?day=${day}`);
-    console.log("lineup success!", response.data);
+    // console.log("lineup success!", response.data);
     return response.data;
 
   } catch (error) {
-    console.error("API error: ", error);
+    // console.error("API error: ", error);
 
     throw error;
   }
@@ -16,11 +16,11 @@ export const fetchArtists = async (day) => {
 export const fetchPlaylists = async (artist_id)=>{
   try {
     const response = await API.get(`api/v1/artist/${artist_id}`);
-    console.log("playlists success!", response.data);
+    // console.log("playlists success!", response.data);
     return response.data;
 
   } catch (error) {
-    console.error("API error: ", error);
+    // console.error("API error: ", error);
 
     throw error;
   }
