1568b66b07e29d76bc80bd57c787fe38ea034de7
coldweek3 <ohchanju3@naver.com>
2024-05-27 15:49:45 +0900

    :bug: package.json 수정

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,24 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    minify: true,
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
-  },
 });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     minify: true,
+//     terserOptions: {
+//       compress: {
+//         drop_console: true,
+//         drop_debugger: true,
+//       },
+//     },
+//   },
+// });
